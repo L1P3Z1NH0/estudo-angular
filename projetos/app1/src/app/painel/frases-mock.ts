430fce53c1ac2a843c0a3d085016ae7ec4f3f1ef
L1P3Z1NH0 <felipecarrapeiro@hotmail.com>
2021-03-07 14:31:45 -0300

    app1Finalizado

---
+++ projetos/app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[] = [
+  { fraseEng: 'I like to learn' , frasePtBr: 'EU GOSTO DE APRENDER'},
+  { fraseEng: 'I watch TV' , frasePtBr: 'EU ASSISTO TV'},
+  { fraseEng: 'How are you ?' , frasePtBr: 'COMO VAI VOCÊ ?'},
+  { fraseEng: 'I eat bread' , frasePtBr: 'EU COMO PÃO'}
+]
--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, OnChanges} from '@angular/core';
+import { Coracao } from '../shared/coracao.model'
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() public tentativas:number = 3
+
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ]
+
+  constructor() {
+
+  }
+
+  ngOnChanges():void{
+    //this.tentativas
+    //this.coracoes.length
+    if(this.tentativas !== this.coracoes.length){
+      let indice = this.coracoes.length - this.tentativas
+
+      //3 - 2 = 1 - 1 = 0
+      //3 - 1 = 2 - 1 = 1
+      //3 - 0 = 3 - 1 = 2
+
+      this.coracoes[indice - 1].cheio = false
+    }
+  }
 
   ngOnInit(): void {
   }
